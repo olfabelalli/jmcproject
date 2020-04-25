33a09d3055b0e5f15c2183b1285dfea8d3f1cfe8
olfabelalli <64318139+olfabelalli@users.noreply.github.com>
2020-04-25 16:39:04 +0200

    Add files via upload

---
+++ checkContact.js
@@ -0,0 +1,20 @@
+function valthisform() {
+    var checkboxs = document.getElementsByName("c1");
+    var okay = false;
+    for (var i = 0, l = checkboxs.length; i < l; i++) {
+        if (checkboxs[i].checked) {
+            okay = true;
+
+            break;
+
+        }
+
+    }
+    if (okay) {
+        // alert("Thank you for checking a checkbox");
+        document.location.href = "contact2.html"
+
+    }
+    else alert("Please check a checkbox");
+}
+
